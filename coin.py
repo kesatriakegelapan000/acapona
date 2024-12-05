33d8d5c7e37983a0c2a000ebca0abf14d2a95dbd
Vryst <uchihaotong17@gmail.com>
2024-12-05 19:24:47 +0700

    dummy

--- coin.py
+++ coin.py
@@ -6,15 +6,15 @@ import os
 os.system("clear")
 
 print("\n\nPilih sisi koin\n1. Kepala\n2. Ekor")
-
-try:
- pilih = int(input("\n\n Pilhan: "))
-except:
- print("\n\nHarap masukan input yang valid(1 atau 2)")
-
-hasil = random.choice([1,2])
-
-if pilih == hasil:
- print("\n\nAnda menang!")
-else:
- print("\n\nAnda kalah, adios.")
+while True:
+ try:
+  pilih = int(input("\n\n Pilhan: "))
+ except:
+  print("\n\nHarap masukan input yang valid(1 atau 2)")
+ 
+ hasil = random.choice([1,2])
+ 
+ if pilih == hasil:
+  print("\n\nAnda menang!")
+ else:
+  print("\n\nAnda kalah, adios.")
