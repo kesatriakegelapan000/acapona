603aa23ddea349e744bcbabd9cd80ac68637bb0c
skytria <vloid137@gmail.com>
2024-12-05 20:18:43 +0700

    gacor88

--- kalkulator.py
+++ kalkulator.py
@@ -3,6 +3,9 @@ import os
 os.system("clear")
 
 print("kalkulator mewing")
-a = input("angka : ")
-b = input("angka : ")
-c = 
+a = float(input("angka : "))
+b = float(input("angka : "))
+c = input("+ atau - : ")
+
+
+print(a+b) if c == "+" else print(a-b)
